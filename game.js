77e8b242a3aaa0faeda7d0b09035d02bef0ec82a
Billy Monks <william.monks@gartner.com>
2019-05-16 13:46:28 -0400

    Added object to represent each gem

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 800,
@@ -9,9 +9,16 @@ var config = {
     }
 };
 
-var gems = [];
+let gems = [];
 
-var game = new Phaser.Game(config);
+const colors = {
+  0: 'red',
+  1: 'yellow',
+  2: 'navy',
+  3: 'aqua'
+};
+
+const game = new Phaser.Game(config);
 
 function preload ()
 {
@@ -26,8 +33,13 @@ function create ()
   for (var i=0; i<8; i++) {
     gems[i] = [];
     for (var j=0; j<8; j++) {
-      gems[i][j] = this.add.sprite(i*100+50, j*100+50 , "gems");
-      gems[i][j].setFrame(Math.floor(Math.random()*6));
+      gems[i][j] =
+        {
+          sprite: this.add.sprite(i*100+50, j*100+50 , "gems"),
+          color: colors[Math.floor(Math.random()*4)],
+          colorIndex: Math.floor(Math.random()*4) //remove when each gem has its own sprite/no longer using setframe
+        };
+      gems[i][j].setFrame(colorIndex);
     }
   }
 }
